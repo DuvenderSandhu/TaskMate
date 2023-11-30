f07346d1ee56a0d794917064e48afe6286f1874b
DuvenderSandhu <85348737+DuvenderSandhu@users.noreply.github.com>
2023-11-30 08:30:49 +0000

    Done

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,7 @@ function Navbar(){
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          <span className="text-white text-xl font-semibold">YourLogo</span>
+          <span className="text-white text-xl font-semibold">Taskmate</span>
         </div>
 
         <div className="flex space-x-4 text-white font-bold">
